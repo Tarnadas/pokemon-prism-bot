b0b04517147f377cc23228eb12b6052f2771989c
Mario <m_reder10@cs.uni-kl.de>
2017-07-28 08:43:35 +0200

    dont break on out of bounds option print all messages improved bsp patch search in channel

--- src/Bot.js
+++ src/Bot.js
@@ -23,24 +23,30 @@ import bspPatch, { BSP_RESULT } from './bsppatch'
 const zipLib = new Zip()
 const unzip = zipLib.extractFull
 
+const UPDATE_BSP_DELAY = 60000
+
 export default class Bot {
   constructor (channelId) {
     // let's just bind everything to prevent errors
     this.onMessage = this.onMessage.bind(this)
     this.initializeBsp = this.initializeBsp.bind(this)
     this.updateBsp = this.updateBsp.bind(this)
+    this.clearUser = this.clearUser.bind(this)
 
     this.client = new Discord.Client()
     this.file = {}
     this.option = {}
     this.waitForOption = {}
+    this.optionSize = {}
     this.isSave = {}
+    this.messagesSent = {}
 
     this.client.on("ready", () => {
       console.log("I am ready!")
       this.channel = this.client.channels.get(channelId)
       this.initializeBsp()
       this.updateBsp()
+      setInterval(this.updateBsp, UPDATE_BSP_DELAY)
     })
 
     this.client.on("message", this.onMessage)
@@ -60,7 +66,7 @@ export default class Bot {
       try {
         this.option[clientId] = parseInt(message.content) - 1
         if (isNaN(this.option[clientId])) throw ''
-        if (this.option[clientId] < 0 || this.option[clientId] > 1) {
+        if (this.option[clientId] < 0 || this.option[clientId] >= this.optionSize[clientId]) {
           message.author.send('Your number is out of bounds.')
           return
         }
@@ -70,8 +76,6 @@ export default class Bot {
         return
       }
     }
-
-    await this.updateBsp()
 
     // loop over sent attachments
     const attachments = message.attachments.array()
@@ -86,7 +90,7 @@ export default class Bot {
     }
 
     if (!this.file[clientId]) {
-      message.author.send(`Hey ${message.author.username}\nI am a Bot, who can update your Crystal ROM to the latest Pokémon Prism release.\nJust send me your dumped ROM and I will do the rest for you.`)
+      message.author.send(`Hey ${message.author.username}\nI am a Bot created by Tarnadas, Retrosol and ax6 who can update your Crystal ROM to the latest Pokémon Prism release.\nJust send me your dumped ROM and I will do the rest for you.`)
       return
     }
     if (!this.bsp || !(this.bsp instanceof ArrayBuffer)) {
@@ -96,35 +100,36 @@ export default class Bot {
 
     // PATCH IT BABY
     try {
-      const [ code, result, messages ] = await bspPatch(this.bsp, this.file[clientId], this.option[clientId])
+      const { code, options, data, messages } = await bspPatch(this.bsp, this.file[clientId], this.option[clientId])
 
+      let resultMessage = ''
+      for (let i = this.messagesSent[clientId] ? this.messagesSent[clientId] : 0; i < messages.length; i++) {
+        resultMessage += `${messages[i]}\n`
+      }
       if (code === BSP_RESULT.MENU && messages) {
-        let resultMessage = ''
-        for (let i in messages) {
-          resultMessage += messages[i] + '\n'
+        for (let i = 0; i < options.length; i++) {
+          resultMessage += `(${i + 1}) ${options[i]} `
         }
-        resultMessage += `(1) ${result[0]} | (2) ${result[1]}\n(please just send the respective number)`
+        resultMessage += '\n(please just send the respective number)'
         if (resultMessage.includes('savefile')) this.isSave[clientId] = true
-        message.author.send(resultMessage)
+        this.optionSize[clientId] = options.length
+        this.messagesSent[clientId] = messages.length
         this.waitForOption[clientId] = true
+        message.author.send(resultMessage)
       } else if (code === BSP_RESULT.SUCCESS) {
+        this.messagesSent[clientId] = messages.length
+        message.author.send(resultMessage)
         message.author.send('Uploading your patched file. Please be patient.')
         message.author.send('Here is your patched file', { files: [
           {
-            attachment: Buffer.from(result),
+            attachment: Buffer.from(data),
             name: `PokemonPrism.${this.isSave[clientId] ? 'sav' : 'gbc'}`
           }
         ]})
-        delete this.file[clientId]
-        delete this.option[clientId]
-        delete this.waitForOption[clientId]
-        delete this.isSave[clientId]
+        this.clearUser(clientId)
       } else if (code === BSP_RESULT.ERROR) {
         message.author.send(`Error: ${result}`)
-        delete this.file[clientId]
-        delete this.option[clientId]
-        delete this.waitForOption[clientId]
-        delete this.isSave[clientId]
+        this.clearUser(clientId)
       }
 
       // send back to user
@@ -132,6 +137,7 @@ export default class Bot {
       // file was not able to be patched
       console.error(err)
       message.author.send(`An internal error occurred: ${err}\nPlease report this error to the author of the Bot.`)
+      this.clearUser(clientId)
     }
   }
 
@@ -144,25 +150,28 @@ export default class Bot {
     }
   }
 
-  async updateBsp () {
-    if (this.doUpdate) return
-    this.doUpdate = true
+  async updateBsp (before) {
     try {
       // loop through last messages and find first occurrence of .rar file
-      const url = await new Promise(async (resolve, reject) => {
+      const [ url, messageId ] = await new Promise(async (resolve, reject) => {
         try {
-          const messages = (await this.channel.fetchMessages()).array()
-          if (messages == null) return
-          for (let i in messages) {
-            const message = messages[i]
-            if (!message.attachments) return
-            const attachments = message.attachments.array()
-            for (let j in attachments) {
-              const attachment = attachments[j]
-              if (!/^.*\.rar$/.test(attachment.filename)) return
-              resolve(attachment.url)
+          const findRar = async (resolve, before) => {
+            const messages = (await this.channel.fetchMessages(before ? { before } : undefined)).array()
+            if (messages == null) return
+            for (let i in messages) {
+              const message = messages[i]
+              if (!message.attachments) return
+              const attachments = message.attachments.array()
+              for (let j in attachments) {
+                const attachment = attachments[j]
+                if (!/^.*\.rar$/.test(attachment.filename)) continue
+                resolve([ attachment.url, message.id ])
+                return
+              }
             }
+            await findRar(resolve, messages[messages.length - 1].id,)
           }
+          await findRar(resolve, before)
           reject('No .bsp patch file found in channel')
         } catch (err) {
           reject(err)
@@ -173,32 +182,45 @@ export default class Bot {
       if (url === this.lastUrl) return
       this.lastUrl = url
 
-      // download .rar file
-      const tmpDir = path.resolve(__dirname, '../tmp')
-      const rar = path.resolve(tmpDir, 'rar')
-      await downloadFile(url, rar)
+      try {
+        // download .rar file
+        const tmpDir = path.resolve(__dirname, '../tmp')
+        const rar = path.resolve(tmpDir, 'rar')
+        await downloadFile(url, rar)
 
-      // decompress .rar file
-      const bsp = path.resolve(tmpDir, 'bsp')
-      if (fs.existsSync(bsp)) {
-        await new Promise((resolve, reject) => {
-          rimraf(bsp, err => {
-            if (err) reject(err)
-            resolve()
+        // decompress .rar file
+        const bsp = path.resolve(tmpDir, 'bsp')
+        if (fs.existsSync(bsp)) {
+          await new Promise((resolve, reject) => {
+            rimraf(bsp, err => {
+              if (err) reject(err)
+              resolve()
+            })
           })
-        })
-      }
-      await unzip(rar, bsp)
+        }
+        await unzip(rar, bsp)
 
-      // read new patch .bsp file
-      const bspBuf = fs.readFileSync(path.resolve(bsp, 'pokeprism.bsp'))
-      this.bsp = bspBuf.buffer
-      console.log(`Received new patch .bsp file with MD5: ${md5(bspBuf)}`)
+        // read new patch .bsp file
+        const bspPath = path.resolve(bsp, 'pokeprism.bsp')
+        if (!fs.existsSync(bspPath)) throw ''
+        const bspBuf = fs.readFileSync(bspPath)
+        this.bsp = bspBuf.buffer
+        console.log(`Received new patch .bsp file with MD5: ${md5(bspBuf)}`)
+      } catch (err) {
+        await this.updateBsp(messageId)
+      }
     } catch (err) {
       console.error(err)
-    } finally {
-      this.doUpdate = false
     }
+  }
+
+  clearUser (clientId) {
+    delete this.file[clientId]
+    delete this.option[clientId]
+    delete this.waitForOption[clientId]
+    delete this.optionSize[clientId]
+    delete this.isSave[clientId]
+    delete this.messagesSent[clientId]
   }
 }
 
